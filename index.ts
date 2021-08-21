15352c90d3c8da06ccaba662f4072883cebb09e2
Anwesh43 <anweshthecool0@gmail.com>
2021-08-22 04:41:20 +0530

    adding constants

---
+++ index.ts
@@ -0,0 +1,8 @@
+const w : number = window.innerWidth
+const h : number = window.innerHeight 
+const rFactor : number = 4.5
+const delay : number = 20 
+const color1 : string = "green"
+const color2 : string = "red"
+const opacity1 : number = 0.6 
+const opacity2 : number = 0.3 
