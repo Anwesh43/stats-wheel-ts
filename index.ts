6dd6859fdcafbd0c763cdc63cca29407e8b180c1
Anwesh43 <anweshthecool0@gmail.com>
2021-08-22 04:55:38 +0530

    adding Stage class

--- index.ts
+++ index.ts
@@ -6,3 +6,22 @@ const color1 : string = "green"
 const color2 : string = "red"
 const opacity1 : number = 0.6 
 const opacity2 : number = 0.3 
+const backColor : string = "#bdbdbd"
+
+class Stage {
+
+    canvas : HTMLCanvasElement = document.createElement('canvas')
+    context : CanvasRenderingContext2D 
+
+    initCanvas() {
+        this.canvas.width = w 
+        this.canvas.height = h 
+        this.context = this.canvas.getContext('2d')
+        document.body.appendChild(this.canvas)
+    }
+    
+    render() {
+        this.context.fillStyle = backColor 
+        this.context.fillRect(0, 0, w, h)
+    }
+}
